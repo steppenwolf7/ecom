0a1cae17810651b56a72521ed8f8bdbcb4e09837
Rafal <rafaltomaszewsky@gmail.com>
2023-08-02 17:44:55 +0200

    new

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
-from .migrations import views
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
--- index/views.py
+++ index/views.py
@@ -1,6 +1,21 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 import requests
 
-
+@login_required
 def index(request):                                             
-    return render(request, 'index.html')
+   return render(request, 'index.html')
+   
+   
+   """ username = request.POST["username"]  # to jest formularz
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    #context = {'username':username}
+    
+    if user is not None:
+        login(request, user)
+        render(request, 'login.html', context=context)
+    else:
+        render(request, 'index.html', context=context)
+        """
+   
