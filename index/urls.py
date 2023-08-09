91a55f1082fd7d597a4cb420c1bdf4b416db639a
Rafal <rafaltomaszewsky@gmail.com>
2023-08-09 19:21:31 +0200

    simple register view

--- index/urls.py
+++ index/urls.py
@@ -5,6 +5,7 @@ from .views import MyLogOutView, MyLoginView
 urlpatterns = [
     path('accounts/logout/', MyLogOutView.as_view(), name='my-logout'),
     path('accounts/login/', MyLoginView.as_view(), name='my-login'),
+    path('accounts/register/', views.register, name='register'),
     path('', views.index, name='index'),
     path("accounts/", include("django.contrib.auth.urls"))
 ]
--- index/views.py
+++ index/views.py
@@ -1,9 +1,25 @@
-#from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LogoutView, LoginView
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
+def register(request):
+   if request.method == "POST":
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+         form.save()
+      messages.success(request, "Your accout is created!")   
+      return redirect('index')
+   else:
+      form = UserCreationForm(request.POST)
+   return render(request, 'register.html', {'form':form})
+
+#@login_required
+def index(request):                                             
+   username = request.user.username
+   context = {'username': username}
+   return render(request, 'index.html', context)
 
 class MyLogOutView(LogoutView):
    template_name = 'registration/logout.html'
@@ -15,11 +31,6 @@ class MyLoginView(LoginView):
    pass
 
 
-@login_required
-def index(request):                                             
-   username = request.user.username
-   context = {'username': username}
-   return render(request, 'index.html', context)
    
    
   
