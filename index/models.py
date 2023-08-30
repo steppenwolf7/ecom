ccc0396b41870cb3849ee9a600a5cf79c9a633de
Rafal <rafaltomaszewsky@gmail.com>
2023-08-30 18:07:53 +0200

    post model & user images

--- index/models.py
+++ index/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    image = models.ImageField(upload_to='user_images/', blank=True, null=True)
