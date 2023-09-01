5756cc549a7048228f1f723d95e48c13dc5a7abb
Rafal <rafaltomaszewsky@gmail.com>
2023-09-01 18:14:36 +0200

    CustomUser

--- index/urls.py
+++ index/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path, include
 from . import views
 from .views import MyLogOutView, MyLoginView
+from django_registration.backends.activation.views import RegistrationView
+
+from index.forms import CustomUserForm
+
 
 urlpatterns = [
     path('accounts/logout/', MyLogOutView.as_view(), name='my-logout'),
     path('accounts/login/', MyLoginView.as_view(), name='my-login'),
     #path('accounts/register/', views.register, name='register'),
     path('', views.index, name='index'),
+    
+    
+    path('accounts/register/', RegistrationView.as_view(form_class=CustomUserForm),name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path("accounts/", include("django.contrib.auth.urls"))
 
