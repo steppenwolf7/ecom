979a811e75938aa2b86793560cece93cb1315bd0
Rafal <rafaltomaszewsky@gmail.com>
2023-09-04 18:01:52 +0200

    registration done

--- ecom/settings.py
+++ ecom/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "index.CustomUser"
+
+ACCOUNT_ACTIVATION_DAYS = False
--- index/urls.py
+++ index/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
+from index.forms import CustomUserForm
 from . import views
 from .views import MyLogOutView, MyLoginView
-from django_registration.backends.activation.views import RegistrationView
+from django.urls import path, include
 
-from index.forms import CustomUserForm
 
 
 urlpatterns = [
@@ -11,10 +11,10 @@ urlpatterns = [
     path('accounts/login/', MyLoginView.as_view(), name='my-login'),
     #path('accounts/register/', views.register, name='register'),
     path('', views.index, name='index'),
-    
-    
-    path('accounts/register/', RegistrationView.as_view(form_class=CustomUserForm),name='django_registration_register'),
+    path('accounts/register/', RegistrationView.as_view(form_class=CustomUserForm, success_url='/'),name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path("accounts/", include("django.contrib.auth.urls"))
+    
+    
 
 ]
