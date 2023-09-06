4f395adb847d5a87a5a9847f11092c38bf97dc48
Rafal <rafaltomaszewsky@gmail.com>
2023-09-07 01:06:26 +0200

    post model

--- postroll/models.py
+++ postroll/models.py
@@ -2,12 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from index.models import CustomUser
 
-"""
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-"""
+
 class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
--- postroll/views.py
+++ postroll/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from .forms import PostForm
+from .models import Post
 
 @login_required
 def postroll(request):                                             
+   if request.method == 'POST':
+        form = PostForm(request.POST) #przekazuje formularz do zmiennej
+        if form.is_valid():
+            # Jeśli formularz jest prawidłowy, zapisz post do bazy danych
+            post = form.save(commit=False) 
+            post.author = request.user  # Przypisz autora do zalogowanego użytkownika
+            post.save()
+            return redirect(request.get_full_path()) 
+   else:
+        form = PostForm() #zapisuje pusty
+   
+   posts = Post.objects.all() 
    username = request.user.username
-   context = {'username': username}
    
-   
+   context = {'username': username,
+              'form': form,
+              'posts': posts}
    
    return render(request, 'postroll.html', context)
